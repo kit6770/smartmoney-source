e9005a5dc3f17aadcccc12061f246e4f5f35a9c4
kitchen <kitchen@sending.me>
2025-02-24 17:59:28 +0800

    remove path prefix

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   // output: 'export',
-  basePath: '/smartmoney',
+  // basePath: '/smartmoney',
   async rewrites() {
     return [
       {
